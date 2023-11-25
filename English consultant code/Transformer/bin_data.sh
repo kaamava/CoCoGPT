5b2d055151205b80335fdc82f12619361c3614aa
kaamava <106901273+kaamava@users.noreply.github.com>
2023-11-25 15:22:36 +0800

    Delete English consultant code directory

--- English consultant code/Transformer/bin_data.sh
+++
@@ -1,6 +0,0 @@
-# Preprocess/binarize the data
-TEXT=data/english
-fairseq-preprocess --source-lang chat_history --target-lang response \
-    --trainpref $TEXT/train --validpref $TEXT/dev --testpref $TEXT/test \
-    --destdir $TEXT/bins \
-    --workers 10
